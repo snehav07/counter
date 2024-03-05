f1a3a7a7207f162a67f3112aacae91cf230cffa1
snehav07 <snehavsunilkumar02@gmail.com>
2024-03-05 11:23:03 +0530

    counter

---
+++ counter/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Counter from './Components/Counter'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1 className='text-center mt-5'>Counter App</h1>
+    <div className=''>
+
+    </div>
+
+      <Counter/>
+    </>
+  )
+}
+
+export default App
